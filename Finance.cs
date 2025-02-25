9312dc46486bc29cdebfc8ee63977ded7d35ed4b
Arya Pratap Singh <aryapratap.singh@optimusinfo.com>
2025-02-25 11:05:18 +0530

    Updated Code

---
+++ Finance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Finance
+{
+	private readonly EmployeeSeperator employeeSeperator;
+	public Finance(Employee employeeSeperator)
+	{
+		this.employeeSeperator = employeeSeperator;
+		//Subscribing event
+		employeeSeperator.EmployeeSeperated += EmployeeSeperatedEventHandler;
+
+    }
+
+	public void EmployeeSeperatedEventHandler()
+	{
+		Console.WriteLine("Finance department: employee seperation process related to finance");
+	}
+}
